378d4c98357b708932f74945a49431b75145a176
Weijia Huang <hhuangweijia@gmail.com>
2013-09-13 15:38:57 -0400

    Second cchange

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<center>
 	
 	<h1>This is the webpage about Weijia (Xavier) Huang</h1>
-<img src="http://www.geohangout.biz/public_html/images/profie picture picture.jpg">
+<img src="picture of me.jpg">
 	
 	<p>
 Weijia (Xavier) Huang is an entrepreneur from Hainan, China. He owns and operates several businesses in China, including a shopping mall and a real estate company. Xavier graduated from B.U. school of entrepreneurial management, and is not studying at Harvard Extension School. Xavier plans to attend business school and expand his businesses between China and the U.S. 
