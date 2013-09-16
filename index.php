5430007bb9cd718c265965e6dd884e780a0615d1
Weijia Huang <hhuangweijia@gmail.com>
2013-09-16 14:46:43 -0400

    added some change

--- index.php
+++ index.php
@@ -2,21 +2,20 @@
 <head>
 
 <title> Welcome to letshangout </title>
-
+<link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
-
-<body bgcolor ="red">
-	
-	<center>
-	
-	<h1>This is the webpage about Weijia (Xavier) Huang</h1>
-<img src="picture of me.jpg">
-	
-	<p>
+<body>
+	<div id ="container">
+	<div id = "header"> <center>
+	<h1>This is the webpage about Weijia (Xavier) Huang</h1> </center></div>
+        <div id = "leftnav"> This is the leftnav </div>
+	<div id = "rightnav"> This is the right nav </div>
+	<div id = "body"></div>	
+        <img src="pictureofme.jpg">
+        </br>
 Weijia (Xavier) Huang is an entrepreneur from Hainan, China. He owns and operates several businesses in China, including a shopping mall and a real estate company. Xavier graduated from B.U. school of entrepreneurial management, and is not studying at Harvard Extension School. Xavier plans to attend business school and expand his businesses between China and the U.S. 
-</p>
 
-	</center>
+   	<div id ="footer"> This is the footer</div>
 
 </body>
 </html>
